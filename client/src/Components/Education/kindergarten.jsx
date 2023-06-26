3ff515882a1db73975a9d40fbbd1c8cf88fff4b2
Ahmed Zakaria <ahmed.zakaria.sera@gmail.com>
2023-06-26 02:17:28 +0200

    updating Education

--- client/src/Components/Education/kindergarten.jsx
+++ client/src/Components/Education/kindergarten.jsx
@@ -47,72 +47,72 @@ const Kindergarten = () => {
           direction: i18n.language === 'ar' ? 'rtl' : 'ltr',
         }}
       >
-      <div className='container-fluid pt-3'>
-        <h2 className="lightGreenColor text-center m-0">
-          {t("Kindergartens")}
-        </h2>
-      </div>
+        <div className='container-fluid pt-3'>
+          <h2 className="lightGreenColor text-center m-0">
+            {t("Kindergartens")}
+          </h2>
+        </div>
 
 
-      <div className='row gy-4 p-0 m-0 w-75 mx-auto'>
-        {Kindergarten.map((res) =>
-          <div key={res.id} className='col-md-5 mx-auto px-0'>
-            <div className='shadow-sm'>
-              <div className='position-relative'>
-                <div className='layer rounded-top-3'></div>
-                <img src={res.img1} alt="" style={{ width: '100%', height: '300px' }} className='rounded-top-3' />
-              </div>
-
-
-              <div className=''>
-                <div className='container pt-3'>
-                  <h4 className='text-center mainColor' >{t(res.name)}</h4>
-                  <p className='text-center'>{t(res.overview)}</p>
+        <div className='row gy-4 p-0 m-0 w-75 mx-auto'>
+          {Kindergarten.map((res) =>
+            <div key={res.id} className='col-md-5 mx-auto px-0'>
+              <div className='shadow-sm'>
+                <div className='position-relative'>
+                  <div className='layer rounded-top-3'></div>
+                  <img src={res.img1} alt="" style={{ width: '100%', height: '300px' }} className='rounded-top-3' />
                 </div>
-                <div className='lightGreenBackgroudColor text-white  rounded-bottom-3'>
-                  <div className='d-flex justify-content-around align-items-center py-3 container'>
-                    <p className='m-0'> {res.Rating} <i className="fa-solid fa-star" style={{ color: '#C3801B' }}></i> </p>
-                    <p className='m-0'><Link to={res.location} className='text-decoration-none text-white'> {t("Location")} </Link></p>
-                    <Link to={res.website} className="text-decoration-none text-white"> {t("Website")} </Link>
-                    <p className='m-0' onClick={() => { const whatsappURL = `https://wa.me/${res.number}`; window.location.href = whatsappURL; }} > <Link className="text-decoration-none text-white" >{t("Phone")}</Link> </p>
 
+
+                <div className=''>
+                  <div className='container pt-3'>
+                    <h4 className='text-center mainColor' >{t(res.name)}</h4>
+                    <p className='text-center'>{t(res.overview)}</p>
                   </div>
-                  <div className='text-center pb-3 '>
-                    <button data-bs-target="#exampleModalToggle" data-bs-toggle="modal"
-                      onClick={() => {
-                        handleShowModal()
-                        setMessage(res.name)
-                      }}
-                      data-whatever="@mdo"
-                      className={i18next.language === 'en' ? "btn lightGreyBg lightGreenColor text-center w-50 rounded-1 btn-mainColor" : " btn-mainColor btn lightGreyBg lightGreenColor  text-center "}
-                    >
-                      {t("Feedback")}
-                    </button>
+                  <div className='lightGreenBackgroudColor text-white  rounded-bottom-3'>
+                    <div className='d-flex justify-content-around align-items-center py-3 container'>
+                      <p className='m-0'> {res.Rating} <i className="fa-solid fa-star" style={{ color: '#C3801B' }}></i> </p>
+                      <p className='m-0'><Link to={res.location} className='text-decoration-none text-white'> {t("Location")} </Link></p>
+                      <Link to={res.website} className="text-decoration-none text-white"> {t("Website")} </Link>
+                      <p className='m-0' onClick={() => { const whatsappURL = `https://wa.me/${res.number}`; window.location.href = whatsappURL; }} > <Link className="text-decoration-none text-white" >{t("Phone")}</Link> </p>
+
+                    </div>
+                    <div className='text-center pb-3 '>
+                      <button data-bs-target="#exampleModalToggle" data-bs-toggle="modal"
+                        onClick={() => {
+                          handleShowModal()
+                          setMessage(res.name)
+                        }}
+                        data-whatever="@mdo"
+                        className={i18next.language === 'en' ? "btn lightGreyBg lightGreenColor text-center w-50 rounded-1 btn-mainColor" : " btn lightGreyBg lightGreenColor text-center w-50 rounded-1 btn-mainColor "}
+                      >
+                        {t("Feedback")}
+                      </button>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          </div>)}
-      </div>
+            </div>)}
+        </div>
 
-      {/* Modal */}
-      <div className="modal fade " id="exampleModalToggle" aria-hidden="true" aria-labelledby="exampleModalToggleLabel" tabIndex="-1" >
-        <div className="modal-dialog modal-dialog-centered">
-          <div className="modal-content">
-            <div className="modal-header lightGreenBackgroudColor text-white text-center">
-              <h1 className="modal-title fs-5 w-100" id="exampleModalLabel">
-                {t("Leave your message")}
-              </h1>
-              <button type="button" className="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close" ></button>
-            </div>
-            <div className="modal-body">
+        {/* Modal */}
+        <div className="modal fade " id="exampleModalToggle" aria-hidden="true" aria-labelledby="exampleModalToggleLabel" tabIndex="-1" >
+          <div className="modal-dialog modal-dialog-centered">
+            <div className="modal-content">
+              <div className="modal-header lightGreenBackgroudColor text-white text-center">
+                <h1 className="modal-title fs-5 w-100" id="exampleModalLabel">
+                  {t("Leave your message")}
+                </h1>
+                <button type="button" className="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close" ></button>
+              </div>
+              <div className="modal-body">
 
-              <FeedBack message={message} ></FeedBack>
+                <FeedBack message={message} ></FeedBack>
+              </div>
             </div>
           </div>
         </div>
       </div>
-</div>
     </>
   );
 };
